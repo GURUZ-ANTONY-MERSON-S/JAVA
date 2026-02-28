d08c63192c6f858e024ffa7917d699137dd7cfb3
GURUZ ANTONY MERSON S <guruzantony000@gmail.com>
2026-02-28 21:14:47 +0530

    Add files via upload

---
+++ DSA_ARRAY.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Main {
+	public static void main(String []  args) {
+		Scanner sc=new Scanner(System.in);
+
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		int min;
+
+		try {
+			for(int i=0; i<n; i++) {
+				arr[i]=sc.nextInt();
+			}
+			min=arr[0];
+
+			for(int i=1; i<n; i++) {
+				if(min>arr[i]) {
+					min=arr[i];
+				}
+			}
+			System.out.println(min);
+			
+		} catch(Exception e) {
+			System.out.println("Error happens, please take a look");
+		}
+	}
+}
